7b4beb300ecb29a27417d511e23d33c4e9b4cccf
BonesPrograms <devinbrown0322@gmail.com>
2026-09-14 11:02:12 -0400

    a

--- obj/Debug/netstandard2.0/XQuinn.AssemblyInfo.cs
+++ obj/Debug/netstandard2.0/XQuinn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1df19a5f1bac44757dc90a93fbaf0faeca127ac4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+818ae695dc666cece3d6a795e8d6b5922de3548b")]
 [assembly: System.Reflection.AssemblyProductAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
