eebf63a2faec838137e5c999913f75ef72636a5f
BonesPrograms <devinbrown0322@gmail.com>
2026-09-19 19:24:11 -0400

    Finished. the Runtime Navigator is complete. The last bit of bugs related to smart enum and generic whitespace skipping are gone. All planned features are have been fully implemented. For the time being, there is nothing more to add. Current projections for new improvements include nested member access, overload resolution, and member access off of method returns. I consider these beyond the current scope of the Runtime Navigator so do not expect to see them.

--- obj/Debug/net10.0-windows/win-x64/XQuinn.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/win-x64/XQuinn.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+288d4b1e35f533271ee1cb46596d5d5e13fa39c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae2fd7412cf82d4c8e78ed246aac98825bdee5b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyTitleAttribute("XQuinn")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
